d4934e14f27063be972af6e27a75c72bc9b5ecfc
theabdulraffay <abdulraffay2494@gmail.com>
2025-04-19 11:46:06 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nascon_prep/firebase_options.dart';
-import 'package:nascon_prep/router/router.dart';
-import 'package:nascon_prep/router/routes.dart';
+
 import 'package:navigation_history_observer/navigation_history_observer.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -25,17 +24,17 @@ class _MyAppState extends State<MyApp> {
   final List<NavigatorObserver> observers = [];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowMaterialGrid: false,
-      navigatorKey: navigatorKey,
-      navigatorObservers: [...observers, NavigationHistoryObserver()],
-      title: 'Flutter Demo',
-      initialRoute: AppRoutes.cloud,
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: onGenerateRoutes,
-      routes: appRoutes,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorKey: navigatorKey,
+        navigatorObservers: [...observers, NavigationHistoryObserver()],
+        title: 'Flutter Demo',
+
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
       ),
     );
   }
