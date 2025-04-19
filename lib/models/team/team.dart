5ce92cd828aba387b648650733acd668ae863085
Abdullah Mehdi <theartistsam47@gmail.com>
2025-04-19 18:18:51 +0500

    feat: user model

--- lib/models/team/team.dart
+++ lib/models/team/team.dart
@@ -4,12 +4,14 @@ class Team extends Job {
   final String id;
   final List<String> members;
   final String name;
+  final String img;
   final bool isPrivate;
 
   const Team({
     required this.id,
     required this.members,
     required this.name,
+    required this.img,
     this.isPrivate = true,
   }) : super(id: id, name: name, members: members);
 
@@ -18,9 +20,11 @@ class Team extends Job {
     List<String>? members,
     String? name,
     bool? isPrivate,
+    String? img,
   }) {
     return Team(
       id: id ?? this.id,
+      img: img ?? this.img,
       members: members ?? this.members,
       name: name ?? this.name,
       isPrivate: isPrivate ?? this.isPrivate,
